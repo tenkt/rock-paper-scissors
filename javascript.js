17b1012a08c5a54793894d21c0d72a7ca75e6409
tenk <119652603+tenkt@users.noreply.github.com>
2023-04-13 16:13:13 -0700

    Fix logic error for getComputerChoice() by adding return value. Add conditions for playRound().

--- javascript.js
+++ javascript.js
@@ -1,13 +1,20 @@
 function playRound(playerSelection, computerSelection) {
-    // your code here!
+    if (computerSelection == playerSelection) {
+        return "Tie!";
+    } else if (computerSelection == "scissor") {
+        return "You win!";
+    } else if (computerSelection == "paper") {
+        return "You Lose!";
+      }
 }
 
 function getComputerChoice() {
-    let myArray = ["Rock", "Paper", "Scissor"];
+    let myArray = ["rock", "paper", "scissor"];
     let randomChoice = myArray[Math.floor(Math.random() * 3)];
-    alert (randomChoice);
+    console.log(randomChoice);
+    return randomChoice;
 }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+ 
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
