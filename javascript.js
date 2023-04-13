c09d9b3fe937c9ad0ef2fc1fb568eac0ff23b740
tenk <119652603+tenkt@users.noreply.github.com>
2023-04-13 02:05:03 -0700

    Create getComputerChoice() random selection generator

--- javascript.js
+++ javascript.js
@@ -1,4 +1,13 @@
-function playGame(){
-    let promptText = prompt("Input rock, paper, or scissors.");
-    alert(promptText);
-};
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+}
+
+function getComputerChoice() {
+    let myArray = ["Rock", "Paper", "Scissor"];
+    let randomChoice = myArray[Math.floor(Math.random() * 3)];
+    alert (randomChoice);
+}
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
