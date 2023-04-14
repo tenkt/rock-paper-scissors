6f3bf80946225059e5b45715c7d5ec916481ca76
tenk <119652603+tenkt@users.noreply.github.com>
2023-04-14 01:19:21 -0700

    Add game().

--- javascript.js
+++ javascript.js
@@ -1,29 +1,32 @@
+const playerSelection = prompt("Input rock, paper or scissors.");
+const computerSelection = getComputerChoice();
+
+function getComputerChoice() {
+    let myArray = ["rock", "paper", "scissors"];
+    let randomChoice = myArray[Math.floor(Math.random() * 3)];
+    return randomChoice;
+}
+
 function playRound(playerSelection, computerSelection) {
-    console.log(`Player throws: ${playerSelection}`);
     if (computerSelection == playerSelection) {
         return "Tie!";
     } else if (playerSelection == "rock" && computerSelection == "scissors" ||
                playerSelection == "paper" && computerSelection == "rock" ||
                playerSelection == "scissors" && computerSelection == "paper") {
-        return "You win!";
+        return "Player wins! Computer Loses!";
     } else if (playerSelection == "rock" && computerSelection == "paper" ||
                playerSelection == "paper" && computerSelection == "scissors" ||
                playerSelection == "scissors" && computerSelection == "rock") {
-        return "You Lose!";
+        return "Computer wins! Player Loses!";
       }
 }
 
-function getComputerChoice() {
-    let myArray = ["rock", "paper", "scissors"];
-    let randomChoice = myArray[Math.floor(Math.random() * 3)];
-    console.log(`Computer throws: ${randomChoice}`);
-    return randomChoice;
-}
-
 function game() {
-
+    console.log(`Player throws: ${playerSelection}`);
+    console.log(`Computer throws: ${computerSelection}`);    
+    return playRound(playerSelection, computerSelection);
 }
 
-const playerSelection = prompt("Input rock, paper or scissors.");
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+game();
+game();
