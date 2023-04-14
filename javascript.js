d76f917f734c470e08dec5662445b4468bd34bd8
tenk <119652603+tenkt@users.noreply.github.com>
2023-04-13 17:00:48 -0700

    Add Computer and Player action strings with template literals. Template literals reflect randomChoice and playerSelection.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 function playRound(playerSelection, computerSelection) {
+    console.log(`Player throws: ${playerSelection}`);
     if (computerSelection == playerSelection) {
         return "Tie!";
     } else if (playerSelection == "rock" && computerSelection == "scissors" ||
@@ -15,7 +16,7 @@ function playRound(playerSelection, computerSelection) {
 function getComputerChoice() {
     let myArray = ["rock", "paper", "scissors"];
     let randomChoice = myArray[Math.floor(Math.random() * 3)];
-    console.log(randomChoice);
+    console.log(`Computer throws: ${randomChoice}`);
     return randomChoice;
 }
 
