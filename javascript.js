e250450374365399f7e10c17cfbd98182a370b4a
tenk <119652603+tenkt@users.noreply.github.com>
2023-04-14 16:01:25 -0700

    Add game scores for each round. Add gameResult() after 5 rounds of game()

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let myArray = ["rock", "paper", "scissors"];
@@ -6,27 +8,40 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    
     if (computerSelection == playerSelection) {
         return "Tie!";
     } else if (playerSelection == "rock" && computerSelection == "scissors" ||
                playerSelection == "paper" && computerSelection == "rock" ||
                playerSelection == "scissors" && computerSelection == "paper") {
-        return "Player wins! Computer Loses!";
+        playerScore++
+        console.log("Player " + playerScore + " " + "|" + " " + "Computer " + computerScore);       
+        return "Player wins this round!";
     } else if (playerSelection == "rock" && computerSelection == "paper" ||
                playerSelection == "paper" && computerSelection == "scissors" ||
                playerSelection == "scissors" && computerSelection == "rock") {
-        return "Computer wins! Player Loses!";
-      }
+        computerScore++
+        console.log("Player " + playerScore + " " + "|" + " " + "Computer " + computerScore);
+        return "Computer wins this round!";
+    }
 }
 
 function game() {
     const playerSelection = prompt("Input rock, paper or scissors.");
     const computerSelection = getComputerChoice();
-
     console.log(`Player throws: ${playerSelection}`);
-    console.log(`Computer throws: ${computerSelection}`);    
+    console.log(`Computer throws: ${computerSelection}`); 
     return playRound(playerSelection, computerSelection);
-    
+    }
+
+function gameResult() {
+    if (playerScore > computerScore) {
+        return "You won!";
+    } else if (playerScore < computerScore) {
+        return "Computer Won!";
+    } else if (playerScore == computerScore) {
+        return "Tie!";
+    }   
 }
 
 
@@ -35,3 +50,6 @@ console.log(game());
 console.log(game());
 console.log(game());
 console.log(game());
+
+console.log("Total Score  Player " + playerScore + " " + "|" + " " + "Computer " + computerScore);
+console.log(gameResult());
